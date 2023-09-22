4859b7b691604da53c9bba7586fefbddfeb4c80e
Harikrishnan Vijayakumaran <h.k.vijayakumaran@gmail.com>
2023-09-22 21:12:40 +0200

    Updated rve info and added post processed output

--- simulations/hyperelastic_simulations/run_hyperelastic_simulation.py
+++ simulations/hyperelastic_simulations/run_hyperelastic_simulation.py
@@ -10,22 +10,32 @@ def run_simulation():
     displacement_gradient = pd.read_csv('doe.csv')
 
     # initialization
-    task1 = HyperelasticRVE()
+    task = HyperelasticRVE()
     # update simulation info
-    task1.update_sim_info(size = 0.048,
-                        radius_mu = 0.002,
-                        radius_std = 0.0004,
-                        vol_req= 0.4,
-                        platform='ubuntu',
-                        seed=17)
+    task.update_sim_info(size=4,
+                         radius_mu=0.2,
+                         radius_std=0.04,
+                         vol_req=0.4,
+                         mesh_division=500,
+                         seed=1234)
 
-    task1_results = {}
+    task_results = pd.DataFrame(columns=['F11', 'F12', 'F21', 'F22', 'P11', 'P12', 'P21', 'P22'])
+
     # calculate responses of simulation
     for ii in range(len(displacement_gradient)):
-        task1_results[ii] = task1.run_simulation(
-            sample={'displacement_gradient': displacement_gradient.iloc[ii].values.reshape(2,2).tolist()}, 
-            third_folder_index=ii
-        )
+
+        results = task.run_simulation(
+            sample={'displacement_gradient': displacement_gradient.iloc[ii].values.reshape(2, 2).tolist()},
+            third_folder_index=ii)
+        required_result_keys = ['deformation_gradient', 'pk1_stress']
+        if set(required_result_keys).issubset(results.keys()):
+            required_results = np.concatenate([results[key][-1].reshape(-1, 4) for key in required_result_keys], axis=1).reshape(-1)
+            required_keys = ['F11', 'F12', 'F21', 'F22', 'P11', 'P12', 'P21', 'P22']
+            required_dict = dict(zip(required_keys, required_results))
+
+            task_results = pd.concat(
+                [task_results, pd.DataFrame([required_dict])], ignore_index=True)
+            task_results.to_csv('output_data.csv', index=False)
 
 if __name__ == "__main__":
     run_simulation()
