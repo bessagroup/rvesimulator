f4140d99345e4c8a7f8603f301ecfdc309dee012
Harikrishnan Vijayakumaran <h.k.vijayakumaran@gmail.com>
2024-02-08 08:24:05 +0100

    Created DoE and updated vf and seed for vf30

--- simulations/rubber_cb_simulations/create_doe.py
+++ simulations/rubber_cb_simulations/create_doe.py
@@ -31,7 +31,7 @@ def get_data_samples(input_dim=3,
 
     number_of_samples = 2**number_of_samples_exponent
     m = number_of_samples_exponent
-    sampler = qmc.Sobol(d=input_dim, scramble=True, seed=1)
+    sampler = qmc.Sobol(d=input_dim, scramble=True, seed=2)
     uniform_samples = sampler.random_base2(m=m)
     assert uniform_samples.shape[0] >= number_of_samples
     data_samples = jax.vmap(
@@ -73,17 +73,17 @@ def get_shear_test_data():
 
 if __name__ == '__main__':
 
-    input_data_space = get_data_samples(input_dim=3,
-                                        lower_bounds=[0.5, 0.5, 0],
-                                        upper_bounds=[2, 2, np.pi/2],
-                                        number_of_samples_exponent=10)
+    # input_data_space = get_data_samples(input_dim=3,
+    #                                     lower_bounds=[0.5, 0.5, 0],
+    #                                     upper_bounds=[2, 2, np.pi/2],
+    #                                     number_of_samples_exponent=10)
     
     # input_data_space = get_shear_test_data()
-    # input_data_space = get_uniaxial_test_data()
+    input_data_space = get_uniaxial_test_data()
 
     input_dataframe = pd.DataFrame(input_data_space,
                                    columns=['dU11', 'dU12', 'dU21', 'dU22']).round(4)
     # input_dataframe = input_dataframe.tail(-128).reset_index(drop=True)
-    input_dataframe.to_csv('doe_vf20.csv', index=True)
-    # input_dataframe.to_csv('doe_shear_test_vf20.csv', index=True)
-    # input_dataframe.to_csv('doe_uniaxial_test_vf20.csv', index=True)
+    # input_dataframe.to_csv('doe_vf30.csv', index=True)
+    # input_dataframe.to_csv('doe_shear_test_vf30.csv', index=True)
+    input_dataframe.to_csv('doe_uniaxial_test_vf30.csv', index=True)
