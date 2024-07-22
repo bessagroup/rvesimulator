0e6ff69c269f20918dfce3d58d2631df13ae418a
JiaxiangYi96 <yagafighting@gmail.com>
2024-07-22 22:54:36 +0200

    Ti6Al4V_3D

--- src/rvesimulator/benchmarks/__init__.py
+++ src/rvesimulator/benchmarks/__init__.py
@@ -5,3 +5,5 @@ from .hollow_plate_sve import (ElasticRegularLoads, VonMisesPlasticPathLoads,
                                VonMisesPlasticRegularLoads)
 from .pppe_cohesive_rve import PPPEMixtureCohesive
 from .pppe_no_cohesive_rve import PPPEMixtureNoCohesive
+from .Ti_6Al_4V_2D import Ti6Al4V_2D
+from .Ti_6Al_4V_3D import Ti6Al4V_3D
